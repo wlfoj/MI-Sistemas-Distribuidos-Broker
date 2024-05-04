8f0495b40950db9af033d2e1c893c2c6a2dc2069
Washington Oliveira <wdeoliveiraj@gmail.com>
2024-05-04 16:46:31 -0300

    corrigindo docker

--- dispositivo/config.py
+++ dispositivo/config.py
@@ -6,9 +6,10 @@ import base64
 # Obtém o valor da variável de ambiente 'NOME' ou retorna 'DISP' se não estiver definida
 conf = {
     'key_conn': 'em terra de rei tem ouro', # Para que o broker me aceite
-    "broker_host_ip": 'localhost',#'192.168.0.2',
+    "broker_host_ip": os.getenv('BROKER_IP', 'localhost'),#'localhost',#'192.168.0.2',
     "broker_host_port_udp": 12346,
     "broker_host_port_tcp": 12345,
+    "unit_measurement": os.getenv('UNIT_MEASUREMENT', ''),
     "NAME_DEVICE": os.environ.get('NAME_DEVICE', ' '),
     "key_crypt": base64.urlsafe_b64encode(b'Jupiter ser feliz com Netuno   ,') # Para usar no construtor do FERNET
 }
